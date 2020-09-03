a193374b36e40cd20e3b45a2b0a3daafaa271f5f
Beka <bekamakharoblishvili@gmail.com>
2020-09-03 14:29:57 +0400

    Configured gatsby for markdown blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,45 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+/* eslint-disable comma-dangle */
+/* eslint-disable arrow-parens */
+const path = require('path');
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const blogPostTemplate = path.resolve('src/templates/blog-post.js');
+  // Query for markdown nodes to use in creating pages.
+  // You can query for whatever data you want to create pages for e.g.
+  // products, portfolio items, landing pages, etc.
+  // Variables can be added as the second function parameter
+  return graphql(
+    `
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              html
+              id
+              frontmatter {
+                date
+                path
+                title
+              }
+            }
+          }
+        }
+      }
+    `,
+    { limit: 1000 }
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors;
+    }
+
+    // Create blog post pages.
+    result.data.allMarkdownRemark.edges.forEach(edge => {
+      createPage({
+        // Path for this page — required
+        path: `${edge.node.frontmatter.path}`,
+        component: blogPostTemplate,
+      });
+    });
+  });
+};
