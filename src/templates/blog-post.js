58d2fe2aa8845205a21f27435fb8f87092dddcf9
Beka <bekamakharoblishvili@gmail.com>
2020-09-03 14:37:15 +0400

    Added blog-post inside page

---
+++ src/templates/blog-post.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
+
+import Layout from '../components/layout';
+
+const Template = ({ data }) => {
+  const post = data.markdownRemark;
+
+  return (
+    <Layout>
+      <div>
+        <Link to="/">go back</Link>
+        <h1>{post.frontmatter.title}</h1>
+        <h4>
+          posted by
+          {post.frontmatter.date}
+        </h4>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  );
+};
+
+export const pageQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        path
+        title
+        date
+      }
+    }
+  }
+`;
+
+Template.propTypes = {
+  data: PropTypes.shape({
+    post: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
+
+export default Template;
